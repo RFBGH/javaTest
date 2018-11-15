0915412b35b136eb50cdbeca7a82e19092d158ee
阮锋冰 <134267@nd.com>
2018-11-15 19:28:29 +0800

    contactWith

---
+++ src/main/java/com/rfb/demo/rxjavatest/gc/GCTest.java
@@ -0,0 +1,38 @@
+package com.rfb.demo.rxjavatest.gc;
+
+import com.rfb.demo.rxjavatest.bean.A;
+
+import java.lang.ref.PhantomReference;
+import java.lang.ref.ReferenceQueue;
+
+/**
+ * Created by Administrator on 2018/11/15 0015.
+ */
+public class GCTest {
+
+
+    public static void test(){
+
+        A a = new A(111);
+
+        ReferenceQueue queue = new ReferenceQueue();
+        PhantomReference phantomReference = new PhantomReference(a, queue);
+        a = null;
+
+        System.gc();
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        if(phantomReference.get() == null){
+            System.out.println("recyle");
+        }else{
+            System.out.println("no");
+        }
+        System.out.println(queue.toString());
+
+    }
+
+}
